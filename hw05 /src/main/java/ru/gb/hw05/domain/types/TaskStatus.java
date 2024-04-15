3c2931a5f909ea2e9084634475cb41f81f806b46
boris <bova4fa@gmail.com>
2024-04-15 14:09:42 +0300

    Homework #05, Rest point - JPA - tasks list

---
+++ hw05 /src/main/java/ru/gb/hw05/domain/types/TaskStatus.java
@@ -0,0 +1,7 @@
+package ru.gb.hw05.domain.types;
+
+public enum TaskStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
---
+++ hw05 /src/main/java/ru/gb/hw05/service/TaskService.java
@@ -0,0 +1,49 @@
+package ru.gb.hw05.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.hw05.domain.Task;
+import ru.gb.hw05.domain.types.TaskStatus;
+import ru.gb.hw05.repository.TaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TaskService {
+    private TaskRepository repository;
+
+    public Task addTask(Task task) {
+        repository.save(task);
+        return task;
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public List<Task> getTasksByStatus(TaskStatus status) {
+        return repository.findAll().stream()
+                .filter(x -> x.getStatus() == status)
+                .toList();
+    }
+
+    public Task updateTaskStatus(Long id, Task task) {
+        Optional<Task> optionalTask = repository.findById(id);
+
+        if (optionalTask.isPresent()) {
+            Task destinationTask = optionalTask.get();
+            destinationTask.setStatus(task.getStatus());
+            return repository.save(destinationTask);
+        } else {
+            throw new IllegalArgumentException("No task found with id: " + id);
+        }
+    }
+
+    public void deleteTaskById(Long id) {
+        Task task = repository.findById(id).orElseGet(null);
+        if (task != null) repository.delete(task);
+    }
+
+}
