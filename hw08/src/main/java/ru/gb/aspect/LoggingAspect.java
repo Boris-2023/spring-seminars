753bbe067f279d1a20293cbd38d6015337118333
boris <bova4fa@gmail.com>
2024-05-07 17:08:52 +0300

    Homework #08, aspects in JPA app

--- hw08/src/main/java/ru/gb/aspect/LoggingAspect.java
+++ hw08/src/main/java/ru/gb/aspect/LoggingAspect.java
@@ -1,12 +1,18 @@
 package ru.gb.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
+    @Before(value = "@annotation(TrackUserAction)")
+    public void beforeAdvice(JoinPoint pp) {
+        System.out.println("Method CALL detected: \nMethod name = " + pp.getSignature().getName() + "()");
+    }
     @AfterReturning(value = "@annotation(TrackUserAction)", returning = "returnedValue")
     public void log(Object returnedValue) {
-        System.out.println("Method call detected, returned: " + returnedValue);
+        System.out.println("Return value = " + returnedValue);
     }
 }
